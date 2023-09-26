13ea7c6d717c888b113e5e9f5f9c504d4129724b
unknown <rajashekhargaddam.marolix@gmail.com>
2023-09-26 16:56:04 +0530

    two sum using while loop

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+list_a = [4,5,6,7,8,10,11]
+target = 18
+left = 0
+right = len(list_a) - 1
+while left < right:
+    if list_a[left] + list_a[right] == target:
+        print([left, right])
+        left = left + 1
+    else:
+        left = left + 1
+    if left == right:
+        left = 0
+        right = right - 1
