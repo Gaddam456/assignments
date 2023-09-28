98f178b858f866fd9a923c5d6e9a88f079b859cb
unknown <rajashekhargaddam.marolix@gmail.com>
2023-09-28 23:42:43 +0530

    28th sept assignment

--- 28thSept/factorial.py
+++ 28thSept/factorial.py
@@ -4,9 +4,7 @@ def factorial(x):
     res = 1
     if x > 1:
         res = x * factorial(x-1)
-        return res
-    else:
-        return res
+    return res
 
 result = factorial(n)
 print(result)
