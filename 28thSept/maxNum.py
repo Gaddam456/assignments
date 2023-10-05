c6facc2bfcbb3e835c71afe808c54942ae2371a0
unknown <rajashekhargaddam.marolix@gmail.com>
2023-10-05 17:04:11 +0530

    max number updated

--- 28thSept/maxNum.py
+++ 28thSept/maxNum.py
@@ -7,4 +7,9 @@ def maxNum(a):
             res = a[i]
     print(res)
 
-maxNum(list_a)
+maxNum(list_a) # this is 1st type of finding max number
+
+print(max(list_a)) # this is 2nd type of finding max number
+
+list_a = sorted(list_a)
+print(list_a[-1]) # this is 3rd type of finding max number
